1952bac260db5e193355ff3797d41ceac10a3342
kelvincjr <deco_2004@163.com>
2021-06-28 18:06:19 +0800

    kelvin commit 20210628pm5

--- wechat_bigdata_lgb/lgb_full.py
+++ wechat_bigdata_lgb/lgb_full.py
@@ -89,6 +89,7 @@ def uAUC(labels, preds, user_id_list):
 
 y_list = ['read_comment', 'like', 'click_avatar', 'forward', 'favorite', 'comment', 'follow']
 max_day = 15
+'''
 ## 读取训练集
 train = pd.read_csv('data/wechat_algo_data1/user_action.csv')
 print(train.shape)
@@ -189,7 +190,13 @@ mem_info()
 ## 内存够用的不需要做这一步
 df = reduce_mem(df, [f for f in df.columns if f not in ['date_'] + play_cols + y_list])
 df.to_csv("./data/lgb.csv", index=False)
+'''
 #assert False
+play_cols = [
+    'is_finish', 'play_times', 'play', 'stay'
+]
+df = pd.read_csv("data/lgb.csv")
+df = reduce_mem(df, [f for f in df.columns if f not in ['date_'] + play_cols + y_list])
 train = df[~df['read_comment'].isna()].reset_index(drop=True)
 test = df[df['read_comment'].isna()].reset_index(drop=True)
 cols = [f for f in df.columns if f not in ['date_'] + play_cols + y_list]
