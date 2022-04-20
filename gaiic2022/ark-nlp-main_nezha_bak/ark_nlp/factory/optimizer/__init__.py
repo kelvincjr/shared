fabc3594c6877256924fe5f001e84bed873d7c89
kelvincjr <deco_2004@163.com>
2022-04-20 15:38:07 +0800

    kelvin commit 20220420pm2

--- gaiic2022/ark-nlp-main_nezha_bak/ark_nlp/factory/optimizer/__init__.py
+++ gaiic2022/ark-nlp-main_nezha_bak/ark_nlp/factory/optimizer/__init__.py
@@ -63,7 +63,7 @@ def get_default_optimizer(module, module_name='bert', **kwargs):
         raise ValueError("The default optimizer does not exist")
 
 
-def get_default_bert_optimizer_1(
+def get_default_bert_optimizer(
     module,
     lr: float = 3e-5,
     eps: float = 1e-6,
@@ -84,48 +84,6 @@ def get_default_bert_optimizer_1(
                       weight_decay=weight_decay)
     return optimizer
 
-def get_default_bert_optimizer(
-    module,
-    lr: float = 3e-5,
-    eps: float = 1e-6,
-    correct_bias: bool = True,
-    weight_decay: float = 1e-3,
-):
-    print('===== get_default_bert_optimizer_new =====')
-    # 差分学习率
-    no_decay = ["bias", "LayerNorm.weight"]
-    model_param = list(module.named_parameters())
-
-    bert_param_optimizer = []
-    other_param_optimizer = []
-
-    for name, para in model_param:
-        space = name.split('.')
-        if space[0] == 'bert_module':
-            bert_param_optimizer.append((name, para))
-        else:
-            other_param_optimizer.append((name, para))
-
-    optimizer_grouped_parameters = [
-        # bert other module
-        {"params": [p for n, p in bert_param_optimizer if not any(nd in n for nd in no_decay)],
-         "weight_decay": weight_decay, 'lr': lr},
-        {"params": [p for n, p in bert_param_optimizer if any(nd in n for nd in no_decay)],
-         "weight_decay": 0.0, 'lr': lr},
-
-        # 其他模块，差分学习率
-        {"params": [p for n, p in other_param_optimizer if not any(nd in n for nd in no_decay)],
-         "weight_decay": weight_decay, 'lr': lr*1e2},
-        {"params": [p for n, p in other_param_optimizer if any(nd in n for nd in no_decay)],
-         "weight_decay": 0.0, 'lr': lr*1e2},
-    ]
-
-    optimizer = AdamW(optimizer_grouped_parameters,
-                      lr=lr,
-                      eps=eps,
-                      correct_bias=correct_bias,
-                      weight_decay=weight_decay)
-    return optimizer
 
 def get_default_crf_bert_optimizer(
     module,
